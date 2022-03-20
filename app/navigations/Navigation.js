655d28eb71dd4e33487bf843bc9285d125b9e49d
Roberto <ruidaing@hotmail.com>
2022-03-20 15:02:58 -0500

    [V5] Montando el Sistema de Navegación

--- app/navigations/Navigation.js
+++ app/navigations/Navigation.js
@@ -1,12 +1,46 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-const Navigation = () => {
+//components
+import Restaurants from "../screens/Restaurants";
+import Favorites from "../screens/Favorites";
+import TopRestaurants from "../screens/TopRestaurants";
+import Search from "../screens/Search";
+import Account from "../screens/Account";
+
+const Tab = createBottomTabNavigator();
+
+export default function Navigation() {
   return (
-    <View>
-      <Text>Navigation</Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="restaurants"
+          component={Restaurants}
+          options={{ title: "Restaurantes" }}
+        />
+        <Tab.Screen
+          name="favorites"
+          component={Favorites}
+          options={{ title: "Favoritos" }}
+        />
+        <Tab.Screen
+          name="top-restaurants"
+          component={TopRestaurants}
+          options={{ title: "Top 5" }}
+        />
+        <Tab.Screen
+          name="search"
+          component={Search}
+          options={{ title: "Buscar" }}
+        />
+        <Tab.Screen
+          name="account"
+          component={Account}
+          options={{ title: "Cuenta" }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-};
-
-export default Navigation;
+}
