ccd88ea04442161c2ccfa002a3a068540c83a540
Roberto <ruidaing@hotmail.com>
2022-03-20 18:44:40 -0500

    46. Screen de usuario no logeados

--- app/screens/Account/UserLogged.js
+++ app/screens/Account/UserLogged.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, View, ScrollView, Text, Image } from "react-native";
+import { Button } from "react-native-elements";
 
 export default function UserLogged() {
   return (
