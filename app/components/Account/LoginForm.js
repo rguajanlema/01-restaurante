e7522473557dd82de078f990bdc54d398e3c3b03
rguajan <rguajan@crececonagora.com>
2022-03-21 16:24:45 -0500

    67. Guardando los datos del formulario de login un estado

--- app/components/Account/LoginForm.js
+++ app/components/Account/LoginForm.js
@@ -4,12 +4,21 @@ import { Input, Icon, Button } from "react-native-elements";
 
 export default function LoginForm() {
   const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState(defaultFormValue());
+
+  const onChange = (e, type) => {
+    setFormData({ ...formData, [type]: e.nativeEvent.text });
+  };
+  const onSubmit = () => {
+    console.log(formData);
+  };
 
   return (
     <View style={styles.formContainer}>
       <Input
         placeholder="Correo electronico"
         containerStyle={styles.inputForm}
+        onChange={(e) => onChange(e, "email")}
         rightIcon={
           <Icon
             type="material-community"
@@ -23,6 +32,7 @@ export default function LoginForm() {
         containerStyle={styles.inputForm}
         password={true}
         secureTextEntry={showPassword ? false : true}
+        onChange={(e) => onChange(e, "password")}
         rightIcon={
           <Icon
             type="material-community"
@@ -36,9 +46,17 @@ export default function LoginForm() {
         title="Iniciar sesión"
         containerStyle={styles.btnContainerLogin}
         buttonStyle={styles.btnLogin}
+        onPress={onSubmit}
       />
     </View>
   );
+}
+
+function defaultFormValue() {
+  return {
+    email: "",
+    password: "",
+  };
 }
 
 const styles = StyleSheet.create({
