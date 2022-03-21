bde71c96d35b0a832df9a0bc3234783a64bf5ef4
Roberto <ruidaing@hotmail.com>
2022-03-20 20:20:54 -0500

    52. Estructura de la screen de registro

--- app/screens/Account/Register.js
+++ app/screens/Account/Register.js
@@ -1,12 +1,29 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 
 export default function Register() {
   return (
     <View>
-      <Text>Register..</Text>
+      <Image
+        source={require("../../../assets/img/5-tenedores-letras-icono-logo.png")}
+        resizeMode="center"
+        style={styles.logo}
+      />
+      <View style={styles.viewForm}>
+        <Text>Register FORM</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  logo: {
+    width: "100%",
+    height: 150,
+    marginTop: 20,
+  },
+  viewForm: {
+    marginRight: 40,
+    marginLeft: 40,
+  },
+});
