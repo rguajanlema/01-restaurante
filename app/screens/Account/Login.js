489f9dcd540288ab4d5485833237278cffd4a586
Roberto <ruidaing@hotmail.com>
2022-03-20 19:56:38 -0500

    51. Navegando a la Screen de Registro desde la Screen de Login

--- app/screens/Account/Login.js
+++ app/screens/Account/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyleSheet, ScrollView, View, Text, Image } from "react-native";
 import { Divider } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Login() {
   return (
@@ -21,12 +22,14 @@ export default function Login() {
 }
 
 function CreateAccount() {
+  const navigation = useNavigation();
+
   return (
     <Text style={styles.textRegister}>
       Aun no tienes una cuenta?{" "}
       <Text
         style={styles.btnRegister}
-        onPress={() => console.log("Registro...")}
+        onPress={() => navigation.navigate("register")}
       >
         Registrate
       </Text>
