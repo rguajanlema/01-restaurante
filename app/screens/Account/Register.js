7c400a16327e4d4a12c9bf0e62658020c328ab06
Roberto <ruidaing@hotmail.com>
2022-03-20 21:10:08 -0500

    54. Evitando que el teclado oculte el input

--- app/screens/Account/Register.js
+++ app/screens/Account/Register.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { StyleSheet, View, Text, Image } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import RegisterForm from "../../components/Account/RegisterForm";
 
 export default function Register() {
   return (
-    <View>
+    <KeyboardAwareScrollView>
       <Image
         source={require("../../../assets/img/5-tenedores-letras-icono-logo.png")}
         resizeMode="center"
@@ -13,7 +14,7 @@ export default function Register() {
       <View style={styles.viewForm}>
         <RegisterForm />
       </View>
-    </View>
+    </KeyboardAwareScrollView>
   );
 }
 
