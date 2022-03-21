d2419055ff4f948d7f5dd8faa71ee7a8f52faac3
Roberto <ruidaing@hotmail.com>
2022-03-20 19:04:09 -0500

    48. Añadiendo funcionalidad al botón de Ver tu Perfil

--- app/screens/Account/UserGuest.js
+++ app/screens/Account/UserGuest.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { StyleSheet, View, ScrollView, Text, Image } from "react-native";
 import { Button } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
 export default function UserGuest() {
+  const navigation = useNavigation();
+
   return (
     <ScrollView centerContent={true} style={styles.viewBody}>
       <Image
@@ -21,7 +24,7 @@ export default function UserGuest() {
           title="Ver tu perfil"
           buttonStyle={styles.btnStyle}
           containerStyle={styles.btnContainer}
-          onPress={() => console.log("!!Click!!")}
+          onPress={() => navigation.navigate("login")}
         />
       </View>
     </ScrollView>
