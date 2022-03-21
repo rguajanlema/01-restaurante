e1b1db2c786c9c7d2452096d5f1b946a85b9dc5e
rguajan <rguajan@crececonagora.com>
2022-03-21 18:15:43 -0500

    70. Estructura del panel de usuario

--- app/screens/Account/UserLogged.js
+++ app/screens/Account/UserLogged.js
@@ -1,14 +1,49 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useRef, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Button } from "react-native-elements";
 import { getAuth, signOut } from "firebase/auth";
+import Toast from "react-native-easy-toast";
+import Loading from "../../components/Loading";
 
 const auth = getAuth();
 
 export default function UserLogged() {
+  const [loading, setLoading] = useState(false);
+  const [loadingText, setLoadingText] = useState("");
+  const toastRef = useRef();
+
   return (
-    <View>
-      <Text>UserLogged...</Text>
-      <Button title="Cerrar sesión" onPress={() => signOut(auth)} />
+    <View style={styles.viewUserInfo}>
+      <Text>InfoUser...</Text>
+      <Text>AccountOptions</Text>
+      <Button
+        title="Cerrar sesión"
+        buttonStyle={styles.btnCloseSession}
+        titleStyle={styles.btnCloseSessionText}
+        onPress={() => signOut(auth)}
+      />
+      <Toast ref={toastRef} position="center" opacity={0.9} />
+      <Loading text={loadingText} isVisible={loading} />
     </View>
   );
 }
+const styles = StyleSheet.create({
+  viewUserInfo: {
+    minHeight: "100%",
+    backgroundColor: "#f2f2f2",
+  },
+  btnCloseSession: {
+    marginTop: 30,
+    borderRadius: 0,
+    backgroundColor: "#fff",
+    borderTopWidth: 1,
+    borderTopColor: "#e3e3e3",
+    borderBottomWidth: 1,
+    borderBottomColor: "#e3e3e3",
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  btnCloseSessionText: {
+    color: "#00a680",
+  },
+});
