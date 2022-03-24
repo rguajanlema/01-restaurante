d07eedab1e9cf88df2d3e567ccfbde4307ff2173
rguajan <84406053+robertoguajan@users.noreply.github.com>
2022-03-23 19:36:46 -0500

    77. Subiendo el avatar al Storage

--- app/components/Account/InfoUser.js
+++ app/components/Account/InfoUser.js
@@ -1,8 +1,13 @@
 import React from "react";
-import { StyleSheet, View, Text, CameraRoll } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import { Avatar } from "react-native-elements";
 import * as Permissions from "expo-permissions";
 import * as ImagePicker from "expo-image-picker";
+
+import firebaseApp from "../../utils/firebase";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firestore = getFirestore(firebaseApp);
 
 export default function InfoUser(props) {
   const {
@@ -21,7 +26,31 @@ export default function InfoUser(props) {
         allowsEditing: true,
         aspect: [4, 3],
       });
-      console.log(result);
+      if (result.cancelled) {
+        toastRef.current.show("Has cerrado la seleccion de imagenes");
+      } else {
+        uploadImage(result.uri)
+          .then(() => {
+            console.log("Imagen subida");
+          })
+          .catch(() => {
+            toastRef.current.show("Error al actualizar el avatar");
+          });
+      }
+    }
+  };
+
+  const uploadImage = async (uri) => {
+    const docuRef = doc(firestore, `avatar/${uri}`);
+    const consulta = await getDoc(docuRef);
+    if (consulta.exists()) {
+      const infoDocu = consulta.data();
+      return infoDocu.Avatar;
+    } else {
+      await setDoc(docuRef, uri);
+      const consulta = await getDoc(docuRef);
+      const infoDocu = consulta.data();
+      return infoDocu.Avatar;
     }
   };
 
