e36133cdd46253d2d5c8ff8c0f7943c5c8917ace
Roberto <ruidaing@hotmail.com>
2022-03-27 10:55:49 -0500

    91. Mostrando y ocultando contrasenia con el icono

--- app/components/Account/ChangeEmailForm.js
+++ app/components/Account/ChangeEmailForm.js
@@ -5,6 +5,7 @@ import { Input, Button } from "react-native-elements";
 export default function ChangeEmailForm(props) {
   const { email, setShowModal, toastRef, setRealoaduserInfo } = props;
   const [formData, setFormData] = useState(defaultValue());
+  const [showPassword, setShowPassword] = useState(false);
 
   const onChange = (e, type) => {
     setFormData({ ...formData, [type]: e.nativeEvent.text });
@@ -31,11 +32,12 @@ export default function ChangeEmailForm(props) {
         placeholder="Contrasenia"
         containerStyle={styles.input}
         password={true}
-        secureTextEntry={true}
+        secureTextEntry={showPassword ? false : true}
         rightIcon={{
           type: "material-community",
-          name: "eye-outline",
+          name: showPassword ? "eye-off-outline" : "eye-outline",
           color: "#c2c2c2",
+          onPress: () => setShowPassword(!showPassword),
         }}
         onChange={(e) => onChange(e, "password")}
       />
