38f42b55ed53a8ddf11f3296bb5166cfe2e9447a
rguajan <84406053+robertoguajan@users.noreply.github.com>
2022-03-31 21:50:45 -0500

    121. Obteniendo los últimos 7 restaurantes añadidos

--- app/screens/Restaurants/Restaurants.js
+++ app/screens/Restaurants/Restaurants.js
@@ -1,18 +1,52 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { Icon } from "react-native-elements";
+import { firebaseApp } from "../../utils/firebase";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {
+  getFirestore,
+  doc,
+  getDocs,
+  setDoc,
+  collection,
+  query,
+  orderBy,
+  limit,
+} from "firebase/firestore";
 
 const auth = getAuth();
+const db = getFirestore(firebaseApp);
 
 export default function Restaurants(props) {
   const { navigation } = props;
   const [user, setUser] = useState(null);
-  console.log(props);
+  const [restaurants, setRestaurants] = useState([]);
+  const [totalRestaurants, setTotalRestaurants] = useState(0);
+  const limiteRestaurants = 10;
+  console.log(totalRestaurants);
 
   useEffect(() => {
     onAuthStateChanged(auth, (userInfo) => {
       setUser(userInfo);
+    });
+  }, []);
+
+  useEffect(() => {
+    getDocs(collection(db, "restaurants")).then((snap) => {
+      setTotalRestaurants(snap.size);
+    });
+
+    const resultRestaurants = [];
+
+    getDocs(collection(db, "restaurants")).then((response) => {
+      /*query(
+        response,
+        orderBy("createAt", "desc"),
+        limit(totalRestaurants)
+      ).then((info) => {
+        console.log(info.doc);
+      });*/
+      console.log(response.docs);
     });
   }, []);
 
