83cd3e1f90ffd491bfeb721bbd0c97786565a48a
Roberto <ruidaing@hotmail.com>
2022-04-02 10:30:37 -0500

    131. Añadiendo un Carousel con las imágenes del restaurante

--- app/screens/Restaurants/Restaurant.js
+++ app/screens/Restaurants/Restaurant.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
 import Loading from "../../components/Loading";
+import Carousel from "../../components/Carousel";
 
 import { firebaseApp } from "../../utils/firebase";
 import { getFirestore, getDoc, doc } from "firebase/firestore";
 
 const db = getFirestore(firebaseApp);
+const screenWidth = Dimensions.get("window").width;
 
 export default function Restaurant(props) {
   const { navigation, route } = props;
   const { id, name } = route.params;
-
   const [restaurant, setRestaurant] = useState(null);
 
   navigation.setOptions({ title: name });
@@ -26,10 +27,19 @@ export default function Restaurant(props) {
   if (!restaurant) return <Loading isVisible={true} text="Cargando..." />;
 
   return (
-    <View>
-      <Text>Restaurant info...</Text>
-    </View>
+    <ScrollView vertical style={styles.viewBody}>
+      <Carousel
+        arrayImages={restaurant.images}
+        height={250}
+        width={screenWidth}
+      />
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewBody: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
