31331a8cdf504cdd0f216eb736918df21d97e8a4
rguajan <84406053+robertoguajan@users.noreply.github.com>
2022-04-04 10:44:54 -0500

    137. Pintando sección de usuario logeado y no logeado

---
+++ app/components/Restaurants/ListReviews.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Button, Avatar, Rating } from "react-native-elements";
+
+import { firebaseApp } from "../../utils/firebase";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getFirestore, getDoc, doc } from "firebase/firestore";
+
+const auth = getAuth();
+const db = getFirestore(firebaseApp);
+
+export default function ListReviews(props) {
+  const { navigation, Restaurant, setRating } = props;
+  const [userLogged, setUserLoged] = useState(false);
+  onAuthStateChanged(auth, (user) => {
+    user ? setUserLoged(true) : setUserLoged(false);
+  });
+
+  return (
+    <View>
+      {userLogged ? (
+        <Button
+          title="Escribe una opinión"
+          buttonStyle={styles.btnAddReview}
+          titleStyle={styles.btnTitleAddReview}
+          icon={{
+            type: "material-community",
+            name: "square-edit-outline",
+            color: "#00a680",
+          }}
+        />
+      ) : (
+        <View>
+          <Text
+            style={{ textAlign: "center", color: "#00a680", padding: 20 }}
+            onPress={() => navigation.navigate("login")}
+          >
+            Para escribir un comentario es necesario esta logeado{" "}
+            <Text style={{ fontWeight: "bold" }}>
+              pulsa AQUI para iniciar seseión
+            </Text>
+          </Text>
+        </View>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  btnAddReview: {
+    backgroundColor: "transparent",
+  },
+  btnTitleAddReview: {
+    color: "#00a680",
+  },
+});
