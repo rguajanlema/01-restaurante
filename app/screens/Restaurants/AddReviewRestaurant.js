cea03225f01a0e00c5b0e420ac2351a06e9945a4
rguajan <84406053+robertoguajan@users.noreply.github.com>
2022-04-04 11:23:35 -0500

    139. Formulario para crear comentarios

--- app/screens/Restaurants/AddReviewRestaurant.js
+++ app/screens/Restaurants/AddReviewRestaurant.js
@@ -1,15 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { AirbnbRating, Button, Input } from "react-native-elements";
 
 export default function AddReviewRestaurant(props) {
   const { navigation, route } = props;
   const { idRestaurant } = route.params;
-  console.log(props);
+
   return (
-    <View>
-      <Text>AddReviewRestaurant</Text>
+    <View style={styles.viewBody}>
+      <View style={styles.viewRating}>
+        <AirbnbRating
+          count={5}
+          reviews={["Pesimo", "Deficiente", "Normal", "Muy bueno", "Execlente"]}
+          defaultRating={0}
+          size={35}
+        />
+      </View>
+      <View style={styles.formReview}>
+        <Input placeholder="Titulo" containerStyle={styles.input} />
+        <Input
+          placeholder="Comentario.."
+          multiline={true}
+          inputContainerStyle={styles.textArea}
+        />
+
+        <Button
+          title="Enviar Comentario"
+          containerStyle={styles.btnComentario}
+          buttonStyle={styles.btn}
+        />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewBody: {
+    flex: 1,
+  },
+  viewRating: {
+    height: 110,
+    backgroundColor: "#f2f2f2",
+  },
+  formReview: {
+    flex: 1,
+    alignItems: "center",
+    margin: 10,
+    marginTop: 40,
+  },
+  input: {
+    marginBottom: 10,
+  },
+  textArea: {
+    height: 150,
+    width: "100%",
+    padding: 0,
+    margin: 0,
+  },
+  btnComentario: {
+    flex: 1,
+    justifyContent: "flex-end",
+    marginTop: 20,
+    marginBottom: 10,
+    width: "95%",
+  },
+  btn: {
+    backgroundColor: "#00a680",
+  },
+});
