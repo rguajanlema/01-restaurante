0784bb54b2ca022e08707053db039d958d25e7b9
Roberto <ruidaing@hotmail.com>
2022-04-05 21:56:03 -0500

    158. Obteniendo los 5 restaurantes mejor puntuados

--- app/screens/TopRestaurants.js
+++ app/screens/TopRestaurants.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { View, Text } from "react-native";
+import Toast from "react-native-easy-toast";
 
-const TopRestaurants = () => {
+import { firebaseApp } from "../utils/firebase";
+import {
+  collection,
+  getDocs,
+  getFirestore,
+  limit,
+  orderBy,
+  query,
+} from "firebase/firestore";
+
+const db = getFirestore(firebaseApp);
+
+export default function TopRestaurants(props) {
+  const { navigation } = props;
+  const [restaurants, setRestaurants] = useState([]);
+  const toastRef = useRef();
+  console.log(restaurants);
+  useEffect(() => {
+    const firstQuery = query(
+      collection(db, "restaurants"),
+      orderBy("createAt", "desc"),
+      limit(5)
+    );
+    getDocs(firstQuery).then((response) => {
+      const restauranArray = [];
+      response.forEach((doc) => {
+        const data = doc.data();
+        data.id = doc.id;
+        restauranArray.push(data);
+      });
+      setRestaurants(restauranArray);
+    });
+  }, []);
+
   return (
     <View>
       <Text>TopRestaurants</Text>
+      <Toast ref={toastRef} position="center" opacity={0.9} />
     </View>
   );
-};
-
-export default TopRestaurants;
+}
