99d8a77f7191cd9355b9a758042e83c2ced09e00
Guajan <roberto.guajan@Thoughtworks.com>
2022-04-06 11:42:19 -0500

    163. Ejecutando la búsqueda en firestore

--- app/screens/Search.js
+++ app/screens/Search.js
@@ -1,15 +1,48 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, View, Text, FlatList, Image } from "react-native";
 import { SearchBar, ListItem, Icon } from "react-native-elements";
+import firebaseApp from "../utils/firebase";
+import {
+  getFirestore,
+  collection,
+  query,
+  startAt,
+  endAt,
+  limit,
+  orderBy,
+  getDocs,
+} from "firebase/firestore";
+import { size } from "lodash";
+import Loading from "../components/Loading";
+
+const db = getFirestore(firebaseApp);
 
 export default function Search(props) {
   const { navigation } = props;
-  const [search, setSearch] = useState("");
+  const [searchText, setSearchText] = useState("");
+  const [searchResults, setSearchResults] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const q = query(
+        collection(db, "restaurants"),
+        orderBy("name"),
+        startAt(searchText),
+        endAt(`${searchText}\uf8ff`),
+        limit(20)
+      );
+
+      const querySnapshot = await getDocs(q);
+      setSearchResults(querySnapshot.docs);
+    })();
+  }, [searchText]);
+
   return (
     <View>
       <SearchBar
         placeholder="Busca tu restaurante..."
-        onChangeText={(e) => setSearch(e)}
+        onChangeText={(text) => setSearchText(text)}
+        value={searchText}
         containerStyle={styles.searchBar}
       />
     </View>
