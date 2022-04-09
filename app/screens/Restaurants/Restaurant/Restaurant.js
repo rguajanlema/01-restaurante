763b78fb1714ec818b786ab03e40b4098dd39ba2
Roberto <ruidaing@hotmail.com>
2022-04-08 21:19:12 -0500

    95. Obteniendo los datos del restaurante

--- app/screens/Restaurants/Restaurant/Restaurant.js
+++ app/screens/Restaurants/Restaurant/Restaurant.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text } from "react-native";
+import {
+  doc,
+  onSnapshot,
+  collection,
+  query,
+  where,
+  orderBy,
+} from "firebase/firestore";
+import { db } from "../../../utils";
 import { styles } from "./Restaurant.styles";
 
 export default function Restaurant(props) {
   const { route } = props;
-  console.log(route.params);
+  const [restaurant, setRestaurant] = useState(null);
+
+  useEffect(() => {
+    setRestaurant(null);
+    onSnapshot(doc(db, "restaurants", route.params.id), (doc) => {
+      setRestaurant(doc.data());
+    });
+  }, [route.params.id]);
+
   return (
     <View>
       <Text>Restaurant</Text>
