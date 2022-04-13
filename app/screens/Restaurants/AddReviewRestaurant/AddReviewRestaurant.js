b335e2d957deb3313a1f4a01aa116f74a6c38938
Guajan <roberto.guajan@Thoughtworks.com>
2022-04-13 17:27:01 -0500

    105. Formulario para añadir review

--- app/screens/Restaurants/AddReviewRestaurant/AddReviewRestaurant.js
+++ app/screens/Restaurants/AddReviewRestaurant/AddReviewRestaurant.js
@@ -1,13 +1,43 @@
 import React, { useState, useRef } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
 import { AirbnbRating, Button, Input } from "react-native-elements";
+import { styles } from "./AddReviewRestaurant.styles";
 
 export function AddReviewRestaurant(props) {
   console.log(props);
 
   return (
-    <View>
-      <Text>AddReviewRestaurant</Text>
+    <View style={styles.content}>
+      <View>
+        <View style={styles.ratingContent}>
+          <AirbnbRating
+            count={5}
+            reviews={[
+              "Pesimo",
+              "Deficiente",
+              "Normal",
+              "Muy bueno",
+              "Excelente",
+            ]}
+            defaultRating={0}
+            size={35}
+            onFinishRating={(rating) => console.log(rating)}
+          />
+        </View>
+        <View>
+          <Input placeholder="Titulo" />
+          <Input
+            placeholder="Comentario"
+            multiline
+            inputContainerStyle={styles.comment}
+          />
+        </View>
+      </View>
+      <Button
+        title="Enviar review"
+        containerStyle={styles.btnContainer}
+        buttonStyle={styles.btn}
+      />
     </View>
   );
 }
--- app/screens/Restaurants/AddReviewRestaurant/AddReviewRestaurant.styles.js
+++ app/screens/Restaurants/AddReviewRestaurant/AddReviewRestaurant.styles.js
@@ -1,34 +1,20 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  viewBody: {
+  content: {
     flex: 1,
+    marginHorizontal: 15,
+    justifyContent: "space-between",
   },
-  viewRating: {
-    height: 110,
-    backgroundColor: "#f2f2f2",
+  ratingContent: {
+    height: 160,
+    justifyContent: "center",
   },
-  formReview: {
-    flex: 1,
-    alignItems: "center",
-    margin: 10,
-    marginTop: 40,
-  },
-  input: {
-    marginBottom: 10,
-  },
-  textArea: {
+  comment: {
     height: 150,
-    width: "100%",
-    padding: 0,
-    margin: 0,
   },
-  btnComentario: {
-    flex: 1,
-    justifyContent: "flex-end",
-    marginTop: 20,
-    marginBottom: 10,
-    width: "95%",
+  btnContainer: {
+    marginBottom: 20,
   },
   btn: {
     backgroundColor: "#00a680",
