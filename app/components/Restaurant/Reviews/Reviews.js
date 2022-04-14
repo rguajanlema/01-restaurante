77f0105d971aaed7cb208a48821d881cfffdb6f5
Guajan <roberto.guajan@Thoughtworks.com>
2022-04-14 12:37:57 -0500

    112. Pintando restaurantes

--- app/components/Restaurant/Reviews/Reviews.js
+++ app/components/Restaurant/Reviews/Reviews.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { Text, AirbnbRating, ListItem, Avatar } from "react-native-elements";
 import {
-  doc,
   onSnapshot,
   collection,
   query,
   where,
   orderBy,
 } from "firebase/firestore";
+import { map } from "lodash";
+import { Loading } from "../../Shared";
 import { db } from "../../../utils";
 import { styles } from "./Reviews.styles";
 
@@ -27,9 +29,35 @@ export function Reviews(props) {
     });
   }, []);
 
+  if (!reviews) return <Loading show text="Cargando" />;
   return (
-    <View>
-      <Text>Reviews</Text>
+    <View style={styles.content}>
+      {map(reviews, (review) => {
+        const data = review.data();
+        const createReview = new Date(data.createdAt.seconds * 1000);
+
+        return (
+          <ListItem key={data.id} bottomDivider containerStyle={styles.review}>
+            <Avatar source={{ uri: data.avatar }} size={50} rounded />
+            <ListItem.Content>
+              <ListItem.Title style={styles.title}>{data.title}</ListItem.Title>
+              <View style={styles.subtitle}>
+                <Text style={styles.comment}>{data.comment}</Text>
+                <View style={styles.contentRationgDate}>
+                  <AirbnbRating
+                    defaultRating={data.rating}
+                    showRating={false}
+                    size={15}
+                    isDisabled
+                    starContainerStyle={styles.starContainer}
+                  />
+                  <Text style={styles.date}>2022-04-14 13:23</Text>
+                </View>
+              </View>
+            </ListItem.Content>
+          </ListItem>
+        );
+      })}
     </View>
   );
 }
