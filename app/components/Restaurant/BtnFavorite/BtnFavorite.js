5fff7816ec10ed033fa5a793109a2ee756913890
Guajan <roberto.guajan@Thoughtworks.com>
2022-04-14 15:34:20 -0500

    115. Añadiendo restaurante a favoritos

--- app/components/Restaurant/BtnFavorite/BtnFavorite.js
+++ app/components/Restaurant/BtnFavorite/BtnFavorite.js
@@ -1,13 +1,38 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import { Icon } from "react-native-elements";
+import { getAuth } from "firebase/auth";
+import {
+  doc,
+  setDoc,
+  getDocs,
+  query,
+  where,
+  collection,
+  deleteDoc,
+} from "firebase/firestore";
+import { db } from "../../../utils";
+import { v4 as uuidv4 } from "uuid";
 import { styles } from "./BtnFavorite.styles";
+import { async } from "@firebase/util";
 
 export function BtnFavorite(props) {
   const { idRestaurant } = props;
+  const auth = getAuth();
 
-  const addFavorite = () => {
-    console.log("Andadir a favoritos");
+  const addFavorite = async () => {
+    try {
+      const idFavorite = uuidv4();
+      const data = {
+        id: idFavorite,
+        idRestaurant,
+        idUser: auth.currentUser.uid,
+      };
+
+      await setDoc(doc(db, "favorites", idFavorite), data);
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <View style={styles.content}>
