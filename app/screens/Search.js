09b0d5b746f791fd64aa127e36dcedabaf837ca0
Guajan <roberto.guajan@Thoughtworks.com>
2022-04-15 10:12:29 -0500

    130. Pintando restaurantes

--- app/screens/Search.js
+++ app/screens/Search.js
@@ -45,6 +45,31 @@ export function Search() {
       />
 
       {!searchResults && <Loading show text="Cargando" />}
+      <ScrollView>
+        {size(searchResults) === 0 ? (
+          <View style={{ alignItems: "center", marginTop: 20 }}>
+            <Text>No se han encontrado resultados</Text>
+          </View>
+        ) : (
+          map(searchResults, (item) => {
+            const data = item.data();
+
+            return (
+              <ListItem
+                key={data.id}
+                bottomDivider
+                onPress={() => console.log("Goto")}
+              >
+                <Avatar source={{ uri: data.images[0] }} rounded />
+                <ListItem.Content>
+                  <ListItem.Title>{data.name}</ListItem.Title>
+                </ListItem.Content>
+                <Icon type="material-community" name="chevron-right" />
+              </ListItem>
+            );
+          })
+        )}
+      </ScrollView>
     </>
   );
 }
