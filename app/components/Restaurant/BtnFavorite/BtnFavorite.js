dab9b63b921b6db87aa8aeb160edd754b057d0a7
Guajan <roberto.guajan@Thoughtworks.com>
2022-04-14 19:02:06 -0500

    118. Eliminando restaurante de favoritos

--- app/components/Restaurant/BtnFavorite/BtnFavorite.js
+++ app/components/Restaurant/BtnFavorite/BtnFavorite.js
@@ -12,9 +12,10 @@ import {
   deleteDoc,
 } from "firebase/firestore";
 import { db } from "../../../utils";
-import { size } from "lodash";
+import { size, forEach, forEachRight } from "lodash";
 import { v4 as uuidv4 } from "uuid";
 import { styles } from "./BtnFavorite.styles";
+import { async } from "@firebase/util";
 
 export function BtnFavorite(props) {
   const { idRestaurant } = props;
@@ -63,8 +64,16 @@ export function BtnFavorite(props) {
     }
   };
 
-  const removeFavorite = () => {
-    console.log("Eliminar favorite");
+  const removeFavorite = async () => {
+    try {
+      const response = await getFavorites();
+      forEach(response, async (item) => {
+        await deleteDoc(doc(db, "favorites", item.id));
+      });
+      onReload();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
