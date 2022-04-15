74288182d73f06ef50db734615775db827f01214
Guajan <roberto.guajan@Thoughtworks.com>
2022-04-14 20:05:43 -0500

    120. Obteniendo lista de favoritos

--- app/screens/Favorites.js
+++ app/screens/Favorites.js
@@ -1,15 +1,46 @@
 import React, { useEffect, useState } from "react";
 import { View, Text } from "react-native";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {
+  doc,
+  getDoc,
+  collection,
+  query,
+  where,
+  onSnapshot,
+} from "firebase/firestore";
+import { db } from "../utils";
 import { UserNotLogged } from "../components/Favorites";
 
 export function Favorites() {
   const [hasLogged, setHasLogged] = useState(null);
+  const [restaurants, setRestaurants] = useState(null);
   const auth = getAuth();
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       setHasLogged(user ? true : false);
+    });
+  }, []);
+
+  useEffect(() => {
+    const q = query(
+      collection(db, "favorites"),
+      where("idUser", "==", auth.currentUser.uid)
+    );
+
+    onSnapshot(q, async (snapshot) => {
+      let restaurantArray = [];
+      for await (const item of snapshot.docs) {
+        const data = item.data();
+        const docRef = doc(db, "restaurants", data.idRestaurant);
+        const docSnap = await getDoc(docRef);
+        const newData = docSnap.data();
+        newData.idFavorite = data.id;
+
+        restaurantArray.push(newData);
+      }
+      setRestaurants(restaurantArray);
     });
   }, []);
 
