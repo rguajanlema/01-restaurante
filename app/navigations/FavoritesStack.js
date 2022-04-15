ba69bb36ecdc6e6d3382ddf5c48651c4128f9e29
Guajan <roberto.guajan@Thoughtworks.com>
2022-04-14 19:48:59 -0500

    119. Screen Favoritos usuario no logeado

--- app/navigations/FavoritesStack.js
+++ app/navigations/FavoritesStack.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import Favorites from "../screens/Favorites";
+import { Favorites } from "../screens/Favorites";
 import { screen } from "../utils";
 
 const Stack = createStackNavigator();
